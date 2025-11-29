769b2e373911891cb8e1726173cf29288868fe58
ykisii <ishiiyuuki@gmail.com>
2025-11-29 16:52:36 +0000

    add NAND, OR gate

--- dl/lib/perceptoron/perceptoron.dart
+++ dl/lib/perceptoron/perceptoron.dart
@@ -1,10 +1,25 @@
 import 'package:scidart/numdart.dart';
 
 int and(double x1, double x2) {
-    final x = Array([x1, x2]);
-    final w = Array([0.5, 0.5]);
-    final b = -0.7;
-    final ret = arraySum(x * w) + b;
+  final x = Array([x1, x2]);
+  final w = Array([0.5, 0.5]);
+  final b = -0.7;
+  final ret = arraySum(x * w) + b;
+  return ret <= 0 ? 0 : 1;
+}
 
-    return ret <= 0 ? 0 : 1;
+int nand(double x1, double x2) {
+  final x = Array([x1, x2]);
+  final w = Array([-0.5, -0.5]);
+  final b = 0.7;
+  final ret = arraySum(x * w) + b;
+  return ret <= 0 ? 0 : 1;
+}
+
+int or(double x1, double x2) {
+  final x = Array([x1, x2]);
+  final w = Array([0.5, 0.5]);
+  final b = -0.2;
+  final ret = arraySum(x * w) + b;
+  return ret <= 0 ? 0 : 1;
 }
--- dl/test/perceptoron/perceptoron_test.dart
+++ dl/test/perceptoron/perceptoron_test.dart
@@ -13,5 +13,29 @@ void main() {
   });
   test('AND true, true', () {
     expect(and(1, 1), 1);
-  });  
+  });
+  test('NAND false, false', () {
+    expect(nand(0, 0), 1);
+  });
+  test('NAND false, true', () {
+    expect(nand(0, 1), 1);
+  });
+  test('NAND true, false', () {
+    expect(nand(1, 0), 1);
+  });
+  test('NAND true, true', () {
+    expect(nand(1, 1), 0);
+  });
+  test('OR false, false', () {
+    expect(or(0, 0), 0);
+  });
+  test('OR false, true', () {
+    expect(or(0, 1), 1);
+  });
+  test('OR true, false', () {
+    expect(or(1, 0), 1);
+  });
+  test('OR true, true', () {
+    expect(or(1, 1), 1);
+  });
 }
